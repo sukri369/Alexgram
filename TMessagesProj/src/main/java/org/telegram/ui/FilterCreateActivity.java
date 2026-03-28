3c5c26d3e1d4a099c3118af49ae22f2d33a2d0e0
Alex <alexx62058@gmail.com>
2026-03-29 00:07:31 +0530

    Unlock Premium Emojis in Folder

--- TMessagesProj/src/main/java/org/telegram/ui/FilterCreateActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/FilterCreateActivity.java
@@ -1483,7 +1483,8 @@ public class FilterCreateActivity extends BaseFragment {
                         checkDoneButton(true);
                     }));
                     cell.setIcon(FolderIconHelper.getTabIcon(newFilterEmoticon), newFilterEmoticon);
-                    cell.setAllowEntities(false);
+                    cell.setAllowEntities(true);
+                    cell.editTextEmoji.allowEmojisForNonPremium(true);
                     cell.editTextEmoji.getEditText().setEmojiColor(getThemedColor(Theme.key_featuredStickers_addButton));
                     cell.editTextEmoji.setEmojiViewCacheType(AnimatedEmojiDrawable.CACHE_TYPE_TOGGLEABLE_EDIT);
                     cell.editTextEmoji.setText(newFilterName);
