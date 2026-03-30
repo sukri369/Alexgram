4c567f82ef74aed9c15948b703d7d1c815f72468
Alex <alexx62058@gmail.com>
2026-03-30 09:06:43 +0530

    Remove unused file

--- line_view.py
+++
@@ -1,5 +0,0 @@
-with open('e:/NagramX/TMessagesProj/src/main/java/org/telegram/ui/Components/SharedMediaLayout.java', 'r', encoding='utf-8') as f:
-    lines = f.readlines()
-
-for i in range(3565, 3585):
-    print(f"{i}: {lines[i].strip()}")
