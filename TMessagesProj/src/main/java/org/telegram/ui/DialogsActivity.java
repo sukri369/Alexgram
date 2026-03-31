d9e2702efed9e02bdfa35ef6a9309c8f7f952e7a
Alex <alexx62058@gmail.com>
2026-04-01 00:32:30 +0530

    Fix unwanted gap appeared between the folder tabs and the chat list

--- TMessagesProj/src/main/java/org/telegram/ui/DialogsActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/DialogsActivity.java
@@ -2251,6 +2251,8 @@ public class DialogsActivity extends BaseFragment implements NotificationCenter.
 
                                 if (NekoConfig.openArchiveOnPull.Bool()) {
                                     AndroidUtilities.runOnUIThread(() -> {
+                                        parentPage.archivePullViewState = ARCHIVE_ITEM_STATE_HIDDEN;
+                                        parentPage.layoutManager.scrollToPositionWithOffset(1, 0);
                                         // Open the folder.
                                         // Delay was taken from PullForegroundDrawable::startOutAnimation().
                                         Bundle args = new Bundle();
