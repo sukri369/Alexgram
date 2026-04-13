c8ba5b982b7acf5c6cf21af44643e6d9d82d06a3
alexandeer1 <alexx62058@gmail.com>
2026-04-13 23:26:06 +0530

    Feat: Analytics Dashboard

--- TMessagesProj/src/main/kotlin/xyz/nextalone/nagram/analytics/ui/screens/DashboardScreen.kt
+++ TMessagesProj/src/main/kotlin/xyz/nextalone/nagram/analytics/ui/screens/DashboardScreen.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
