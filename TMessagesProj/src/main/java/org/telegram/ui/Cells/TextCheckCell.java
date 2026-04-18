02270d2875a241728fd8bbd4e28c57cd86a7a9d4
alexandeer1 <alexx62058@gmail.com>
2026-04-18 17:42:48 +0530

    Fix language problems

--- TMessagesProj/src/main/java/org/telegram/ui/Cells/TextCheckCell.java
+++ TMessagesProj/src/main/java/org/telegram/ui/Cells/TextCheckCell.java
@@ -29,6 +29,7 @@ import android.view.View;
 import android.view.accessibility.AccessibilityNodeInfo;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import org.telegram.messenger.AndroidUtilities;
