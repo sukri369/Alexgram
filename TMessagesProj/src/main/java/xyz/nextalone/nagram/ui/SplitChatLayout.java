6b3957d95b82bffe01f206736a2bf3109a638286
alexandeer1 <alexx62058@gmail.com>
2026-04-23 17:07:52 +0530

    feat: Ambient Mode

--- TMessagesProj/src/main/java/xyz/nextalone/nagram/ui/SplitChatLayout.java
+++ TMessagesProj/src/main/java/xyz/nextalone/nagram/ui/SplitChatLayout.java
@@ -14,6 +14,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import android.widget.HorizontalScrollView;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
