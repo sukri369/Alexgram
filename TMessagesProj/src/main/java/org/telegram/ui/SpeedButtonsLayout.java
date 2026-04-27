3c0f949869af45b4b6bda7cd067ce8b20b64a904
alexandeer1 <alexx62058@gmail.com>
2026-04-28 01:56:08 +0530

    Feat: custom playback speed option

--- TMessagesProj/src/main/java/org/telegram/ui/SpeedButtonsLayout.java
+++ TMessagesProj/src/main/java/org/telegram/ui/SpeedButtonsLayout.java
@@ -12,6 +12,7 @@ import android.graphics.drawable.Drawable;
 import android.util.TypedValue;
 import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 
@@ -103,7 +104,7 @@ public class SpeedButtonsLayout extends PopupSwipeBackLayout {
         }
 
         customItem = new ActionBarMenuSubItem(context, false, false);
-        customItem.setTextAndIcon(LocaleController.getString("PollV2PollDurationOptionCustom", R.string.PollV2PollDurationOptionCustom), new GenericPathDrawable("M20 13C20 15.2091 19.1046 17.2091 17.6569 18.6569L19.0711 20.0711C20.8807 18.2614 22 15.7614 22 13 22 7.47715 17.5228 3 12 3 6.47715 3 2 7.47715 2 13 2 15.7614 3.11929 18.2614 4.92893 20.0711L6.34315 18.6569C4.89543 17.2091 4 15.2091 4 13 4 8.58172 7.58172 5 12 5 16.4183 5 20 8.58172 20 13ZM15.293 8.29297 10.793 12.793 12.2072 14.2072 16.7072 9.70718 15.293 8.29297Z", 24, 24));
+        customItem.setTextAndIcon(LocaleController.getString("PollV2PollDurationOptionCustom", R.string.PollV2PollDurationOptionCustom), 0, new GenericPathDrawable("M20 13C20 15.2091 19.1046 17.2091 17.6569 18.6569L19.0711 20.0711C20.8807 18.2614 22 15.7614 22 13 22 7.47715 17.5228 3 12 3 6.47715 3 2 7.47715 2 13 2 15.7614 3.11929 18.2614 4.92893 20.0711L6.34315 18.6569C4.89543 17.2091 4 15.2091 4 13 4 8.58172 7.58172 5 12 5 16.4183 5 20 8.58172 20 13ZM15.293 8.29297 10.793 12.793 12.2072 14.2072 16.7072 9.70718 15.293 8.29297Z", 24, 24));
         customItem.setColors(0xffffffff, 0xffffffff);
         customItem.setOnClickListener((view) -> {
             openForeground(1);
@@ -258,7 +259,7 @@ public class SpeedButtonsLayout extends PopupSwipeBackLayout {
 
         @Override
         public int getIntrinsicHeight() {
-            return AndroidUtilities.dp(vHeight);
+            return AndroidUtilities.dp(height);
         }
     }
 }
