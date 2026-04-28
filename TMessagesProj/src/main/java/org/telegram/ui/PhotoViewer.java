703b7ba04652e0b1a6d198dc7be33e013393ac9d
alexandeer1 <alexx62058@gmail.com>
2026-04-28 10:45:32 +0530

    Feat: custom playback speed option

--- TMessagesProj/src/main/java/org/telegram/ui/PhotoViewer.java
+++ TMessagesProj/src/main/java/org/telegram/ui/PhotoViewer.java
@@ -6294,9 +6294,6 @@ public class PhotoViewer implements NotificationCenter.NotificationCenterDelegat
         videoItem.getPopupLayout().setSwipeBackForegroundColor(0xff222222);
         videoItem.getPopupLayout().swipeBackGravityRight = true;
         videoItem.getPopupLayout().setFitItems(true);
-        videoItem.setLayoutInScreen(true);
-        videoItem.setSubMenuOpenSide(0);
-        videoItem.setMenuXOffset(dp(-8));
 
         speedItem = new ActionBarMenuSlider.SpeedSlider(activityContext, resourcesProvider);
         speedItem.setStops(new float[]{0.5f, 1.0f, 1.5f, 2.0f, 2.5f});
