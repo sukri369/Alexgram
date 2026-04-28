e012eb83412a9575e0694c4f47084f92e143e3bc
alexandeer1 <alexx62058@gmail.com>
2026-04-28 09:31:58 +0530

    Feat: custom playback speed option

--- TMessagesProj/src/main/java/org/telegram/ui/SpeedButtonsLayout.java
+++ TMessagesProj/src/main/java/org/telegram/ui/SpeedButtonsLayout.java
@@ -9,12 +9,11 @@ import android.graphics.Path;
 import android.graphics.PixelFormat;
 import android.graphics.Rect;
 import android.graphics.drawable.Drawable;
-import android.util.TypedValue;
 import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.FrameLayout;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.core.graphics.ColorUtils;
 import androidx.core.graphics.PathParser;
@@ -22,64 +21,23 @@ import androidx.core.math.MathUtils;
 
 import org.telegram.messenger.AndroidUtilities;
 import org.telegram.messenger.LocaleController;
-import org.telegram.messenger.MediaController;
 import org.telegram.messenger.R;
-import org.telegram.ui.ActionBar.ActionBarMenu;
 import org.telegram.ui.ActionBar.ActionBarMenuItem;
 import org.telegram.ui.ActionBar.ActionBarMenuSlider;
 import org.telegram.ui.ActionBar.ActionBarMenuSubItem;
-import org.telegram.ui.ActionBar.ActionBarPopupWindow;
 import org.telegram.ui.ActionBar.Theme;
 import org.telegram.ui.Components.LayoutHelper;
-import org.telegram.ui.Components.PopupSwipeBackLayout;
 import org.telegram.ui.Components.SpeedIconDrawable;
 
-public class SpeedButtonsLayout extends PopupSwipeBackLayout {
+public class SpeedButtonsLayout {
 
-    private final LinearLayout mainLayout;
-    private final LinearLayout customLayout;
-    private final CustomSpeedSlider speedSlider;
-    private final ActionBarMenuSubItem customItem;
     private final ActionBarMenuSubItem[] speedItems = new ActionBarMenuSubItem[5];
+    private final ActionBarMenuSubItem customItem;
+    private final CustomSpeedSlider speedSlider;
     private final Callback callback;
 
-    public SpeedButtonsLayout(Context context, Callback callback) {
-        super(context, null);
+    public SpeedButtonsLayout(Context context, ActionBarMenuItem parentItem, Callback callback) {
         this.callback = callback;
-
-        mainLayout = new LinearLayout(context);
-        mainLayout.setOrientation(LinearLayout.VERTICAL);
-
-        addOnSwipeBackProgressListener((layout, toProgress, progress) -> {
-            if (getParent() instanceof ViewGroup) {
-                ViewGroup parent = (ViewGroup) getParent();
-                for (int i = 0; i < parent.getChildCount(); i++) {
-                    View child = parent.getChildAt(i);
-                    if (child != this) {
-                        child.setAlpha(Math.max(0f, 1f - progress));
-                        child.setTranslationY(AndroidUtilities.dp(16) * progress);
-                        if (progress >= 1f) {
-                            if (child.getVisibility() != GONE) {
-                                child.setVisibility(GONE);
-                            }
-                        } else {
-                            if (child.getVisibility() != VISIBLE) {
-                                child.setVisibility(VISIBLE);
-                            }
-                        }
-                        if (progress > 0 && progress < 1) {
-                            if (child.getLayerType() != View.LAYER_TYPE_HARDWARE) {
-                                child.setLayerType(View.LAYER_TYPE_HARDWARE, null);
-                            }
-                        } else {
-                            if (child.getLayerType() != View.LAYER_TYPE_NONE) {
-                                child.setLayerType(View.LAYER_TYPE_NONE, null);
-                            }
-                        }
-                    }
-                }
-            }
-        });
 
         for (int i = 0; i < 5; i++) {
             int icon;
@@ -107,7 +65,7 @@ public class SpeedButtonsLayout extends PopupSwipeBackLayout {
                 speed = 2.0f;
             }
 
-            ActionBarMenuSubItem item = ActionBarMenuItem.addItem(mainLayout, icon, text, false, null);
+            ActionBarMenuSubItem item = parentItem.addSubItem(0, icon, text);
             item.setColors(0xffffffff, 0xffffffff);
             item.setOnClickListener((view) -> {
                 callback.onSpeedSelected(speed, true, true);
@@ -116,55 +74,44 @@ public class SpeedButtonsLayout extends PopupSwipeBackLayout {
             speedItems[i] = item;
         }
 
-        customItem = new ActionBarMenuSubItem(context, false, false);
-        customItem.setTextAndIcon(LocaleController.getString("PollV2PollDurationOptionCustom", R.string.PollV2PollDurationOptionCustom), 0, new GenericPathDrawable("M20 13C20 15.2091 19.1046 17.2091 17.6569 18.6569L19.0711 20.0711C20.8807 18.2614 22 15.7614 22 13 22 7.47715 17.5228 3 12 3 6.47715 3 2 7.47715 2 13 2 15.7614 3.11929 18.2614 4.92893 20.0711L6.34315 18.6569C4.89543 17.2091 4 15.2091 4 13 4 8.58172 7.58172 5 12 5 16.4183 5 20 8.58172 20 13ZM15.293 8.29297 10.793 12.793 12.2072 14.2072 16.7072 9.70718 15.293 8.29297Z", 24, 24));
-        customItem.setColors(0xffffffff, 0xffffffff);
-        customItem.setOnClickListener((view) -> {
-            openForeground(1);
-            try {
-                view.performHapticFeedback(android.view.HapticFeedbackConstants.KEYBOARD_TAP);
-            } catch (Exception ignore) {}
-        });
-        customItem.setSelectorColor(0x1affffff);
-        mainLayout.addView(customItem, LayoutHelper.createLinear(LayoutHelper.MATCH_PARENT, 48));
-
-        addView(mainLayout, LayoutHelper.createFrame(LayoutHelper.MATCH_PARENT, LayoutHelper.WRAP_CONTENT));
-
-        customLayout = new LinearLayout(context);
-        customLayout.setOrientation(LinearLayout.VERTICAL);
-        customLayout.setPadding(0, AndroidUtilities.dp(4), 0, AndroidUtilities.dp(12));
+        // Custom Slider Container
+        LinearLayout sliderContainer = new LinearLayout(context);
+        sliderContainer.setOrientation(LinearLayout.VERTICAL);
+        sliderContainer.setPadding(0, 0, 0, AndroidUtilities.dp(8));
+        sliderContainer.setBackgroundColor(0xff222222);
 
         ActionBarMenuSubItem backItem = new ActionBarMenuSubItem(context, false, false);
         backItem.setTextAndIcon(LocaleController.getString("Back", R.string.Back), R.drawable.ic_ab_back);
         backItem.setColors(0xffffffff, 0xffffffff);
-        backItem.setOnClickListener((view) -> closeForeground());
+        backItem.setOnClickListener((view) -> {
+            if (parentItem.getPopupLayout() != null) {
+                parentItem.getPopupLayout().getSwipeBackLayout().closeForeground();
+            }
+        });
         backItem.setSelectorColor(0x1affffff);
-        customLayout.addView(backItem, LayoutHelper.createLinear(LayoutHelper.MATCH_PARENT, 48));
+        sliderContainer.addView(backItem, LayoutHelper.createLinear(LayoutHelper.MATCH_PARENT, 48));
 
-        View headerView = new View(context) {
-            private final Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
-            private final Paint textPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-            {
-                textPaint.setColor(0xff888888);
-                textPaint.setTextSize(AndroidUtilities.dp(12));
-                textPaint.setTypeface(AndroidUtilities.getTypeface("fonts/rmedium.ttf"));
-            }
+        View headerSeparator = new View(context);
+        headerSeparator.setBackgroundColor(0xff181818);
+        sliderContainer.addView(headerSeparator, LayoutHelper.createLinear(LayoutHelper.MATCH_PARENT, 1));
 
-            @Override
-            protected void onDraw(Canvas canvas) {
-                String text = LocaleController.getString("PollV2PollDurationOptionCustom", R.string.PollV2PollDurationOptionCustom).toUpperCase();
-                canvas.drawText(text, AndroidUtilities.dp(21), AndroidUtilities.dp(18), textPaint);
-            }
-        };
-        customLayout.addView(headerView, LayoutHelper.createLinear(LayoutHelper.MATCH_PARENT, 24));
+        TextView header = new TextView(context);
+        header.setText(LocaleController.getString("PollV2PollDurationOptionCustom", R.string.PollV2PollDurationOptionCustom).toUpperCase());
+        header.setTextColor(0xff888888);
+        header.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 12);
+        header.setTypeface(AndroidUtilities.getTypeface("fonts/rmedium.ttf"));
+        header.setPadding(AndroidUtilities.dp(21), AndroidUtilities.dp(16), AndroidUtilities.dp(21), AndroidUtilities.dp(8));
+        sliderContainer.addView(header);
 
         speedSlider = new CustomSpeedSlider(context, null);
         speedSlider.setOnValueChange((value, isFinal) -> {
             callback.onSpeedSelected(speedSlider.getSpeed(value), isFinal, false);
         });
-        customLayout.addView(speedSlider, LayoutHelper.createLinear(LayoutHelper.MATCH_PARENT, 50, 4, 0, 4, 0));
+        sliderContainer.addView(speedSlider, LayoutHelper.createLinear(LayoutHelper.MATCH_PARENT, 50, 4, 0, 4, 4));
 
-        addView(customLayout, LayoutHelper.createFrame(LayoutHelper.MATCH_PARENT, LayoutHelper.WRAP_CONTENT));
+        customItem = parentItem.addSwipeBackItem(0, new GenericPathDrawable("M20 13C20 15.2091 19.1046 17.2091 17.6569 18.6569L19.0711 20.0711C20.8807 18.2614 22 15.7614 22 13 22 7.47715 17.5228 3 12 3 6.47715 3 2 7.47715 2 13 2 15.7614 3.11929 18.2614 4.92893 20.0711L6.34315 18.6569C4.89543 17.2091 4 15.2091 4 13 4 8.58172 7.58172 5 12 5 16.4183 5 20 8.58172 20 13ZM15.293 8.29297 10.793 12.793 12.2072 14.2072 16.7072 9.70718 15.293 8.29297Z", 24, 24), LocaleController.getString("PollV2PollDurationOptionCustom", R.string.PollV2PollDurationOptionCustom), sliderContainer);
+        customItem.setColors(0xffffffff, 0xffffffff);
+        customItem.setSelectorColor(0x1affffff);
     }
 
     public void update(float currentVideoSpeed, boolean isFinal) {
@@ -192,12 +139,18 @@ public class SpeedButtonsLayout extends PopupSwipeBackLayout {
         speedSlider.setSpeed(currentVideoSpeed, false);
     }
 
+    public void setVisibility(int visibility) {
+        for (ActionBarMenuSubItem item : speedItems) {
+            item.setVisibility(visibility);
+        }
+        customItem.setVisibility(visibility);
+    }
+
     public interface Callback {
         void onSpeedSelected(float speed, boolean isFinal, boolean closeMenu);
     }
 
     private static class CustomSpeedSlider extends ActionBarMenuSlider.SpeedSlider {
-
         public static final float MIN_SPEED = 0.2f;
         public static final float MAX_SPEED = 16.0f;
 
@@ -237,14 +190,6 @@ public class SpeedButtonsLayout extends PopupSwipeBackLayout {
                 return ColorUtils.blendARGB(0xffffffff, 0xff6BB6F9, MathUtils.clamp((speed - 0.2f) / 0.8f, 0, 1));
             }
             return ColorUtils.blendARGB(0xff6BB6F9, 0xff3196f0, MathUtils.clamp((speed - 1f) / 15f, 0, 1));
-        }
-
-        @Override
-        public void invalidate() {
-            super.invalidate();
-            if (getParent() != null) {
-                ((View) getParent()).invalidate();
-            }
         }
     }
 
