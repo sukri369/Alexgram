4cfa1807a00071b0567bf2fc417a29b9214efc48
raushan-maker <raushan99goh@gmail.com>
2026-05-25 14:44:36 +0530

    feat: add NekoSettingsActivity with integrated search and cloud sync features

--- TMessagesProj/src/main/java/tw/nekomimi/nekogram/settings/NekoSettingsActivity.java
+++ TMessagesProj/src/main/java/tw/nekomimi/nekogram/settings/NekoSettingsActivity.java
@@ -14,6 +14,7 @@ import android.graphics.Color;
 import android.graphics.PorterDuff;
 import android.graphics.PorterDuffColorFilter;
 import android.net.Uri;
+import android.util.TypedValue;
 import android.os.Build;
 import android.text.Editable;
 import android.text.InputType;
